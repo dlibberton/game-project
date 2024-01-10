ad5504b4983191fe9a9333098ebe4348e63c7a11
Drew Libberton <drew.libberton@gmail.com>
2024-01-09 20:20:59 -0500

    git commit

--- test.js
+++ test.js
@@ -4,5 +4,14 @@ U-U-U-U
 -X-X-X-
 T-T-T-T
 =======
-objects at T
+objects at T will collide with other users board T
+
+const tUnit = { health(number), damage(number) } other attributes possible
+const xUnit = { health(number), damage(number) } 
+X unit unique traits may be different from t units
+X units jump to U units at certain times
+others remain stationary and asses combat values directly across from each other
+when unit across does not exist, unit reasses based on proximity
+events may trigger granting modifiers to existing attributes
+unique attributes stored in object can have a values of 0 until iterated by specialEvent
 */ 
